eabd5ded47825caa5a486c6910d0142b108a4f4a
giselle <seauyeansu@gmail.com>
2020-11-06 22:48:35 +0100

    tween

---
+++ components/animation/profiletexttween.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { gsap } from 'gsap';
+import { TimelineMax, TweenMax, CSSPlugin, ScrollToPlugin, Draggable } from "gsap"; 
+
+
+
+class ProfiletextTween extends Component {
+
+	constructor(props){
+		super(props);
+		// profile text container
+		this.ptContainer = null;
+		// profile text tween
+		this.ptTween = null;
+	}
+
+	componentDidMount(){
+		// use the node ref to create the animation
+		this.ptContainer = document.getElementById("profiletext");
+    	this.ptTween = gsap.from(this.ptContainer, 2, {x: 150});
+	}
+
+	render() {
+		return (
+			<div ref={div => this.ptContainer = div} />
+		)		
+	}	
+
+}
+
+export default ProfiletextTween;
--- components/body/body.js
+++ components/body/body.js
@@ -1,34 +1,27 @@
 import React from 'react'
-import profilepic from './profilepic.jpg';
 import './body.css';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import { spacing } from '@material-ui/system';
-import { height } from '@material-ui/system';
-import { whiteSpace } from '@material-ui/core/Box'
+import { whiteSpace } from '@material-ui/core/Box';
+import profilepic from './profilepic.jpg';
+import ProfiletextTween from '../animation/profiletexttween.js';
 
 
 const Body = () => {
 	return (
 		<div>
-		<Box height = {200} component="div" whiteSpace="nowrap"></Box>
+		<Box height = {170} whiteSpace="nowrap"></Box>
 		<Grid container>
-     		<Grid item xs height='400px' className = 'col1'> 
-         	 	<img src = {profilepic} alt = "pp" style = {{
-			  	borderRadius: '50%',
-			  	}} />
+     		<Grid item xs={6} className = 'col1'> 
+         	 	<img 
+				src = {profilepic} 
+				alt = "" 
+				style = {{borderRadius: '50%'}}
+				/>
      		</Grid>
-     		<Grid item xs height='400px' className = 'col2'>
-     			Hello World!
-    	 	</Grid>
-		</Grid>
-
-		<Grid container>
-     		<Grid item xs={6} className = 'col3'> 
-         	 	<h1>I'm Web Developer Giselle Su</h1>
-     		</Grid>
-     		<Grid item xs={6} className = 'col4'>
-     		Hello World!
+     		<Grid item xs={6} className = 'col2'>
+     			<div id="profiletext">I'm Web Developer Giselle Su.</div>
+         	 	<ProfiletextTween />
     		 </Grid>
 		</Grid>
 		</div>
